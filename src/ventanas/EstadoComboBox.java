3cd87891a540ff1bc930589f23baf17d0234a979
Mauricio Castro Ugarte <98657277+C4STR000@users.noreply.github.com>
2024-06-07 13:46:17 -0400

    Update EstadoComboBox.java

--- src/ventanas/EstadoComboBox.java
+++ src/ventanas/EstadoComboBox.java
@@ -1,22 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ventanas;
 
 /**
- *
- * @author Daniel
+ * Clase que gestiona el estado del índice seleccionado en un ComboBox.
+ * Esta clase es estática y mantiene un único estado para todo el programa.
+ * 
+ * Permite establecer y obtener el índice seleccionado en un ComboBox.
+ * 
+ * Ejemplo de uso:
+ * - Para establecer el índice seleccionado: EstadoComboBox.setIndiceSeleccionado(2);
+ * - Para obtener el índice seleccionado: int indice = EstadoComboBox.getIndiceSeleccionado();
+ * 
+ * El índice seleccionado es inicialmente 0.
  */
 public class EstadoComboBox {
    
-    
     private static int indiceSeleccionado = 0;
 
+    /**
+     * Establece el índice seleccionado en el ComboBox.
+     * 
+     * @param indice Índice a establecer como seleccionado.
+     */
     public static void setIndiceSeleccionado(int indice) {
         indiceSeleccionado = indice;
     }
 
+    /**
+     * Obtiene el índice seleccionado en el ComboBox.
+     * 
+     * @return Índice seleccionado.
+     */
     public static int getIndiceSeleccionado() {
         return indiceSeleccionado;
     }
